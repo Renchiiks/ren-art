ca287a689a56d3246728e5ab8f61c18575f31e88
Renchiiks <renate.kalmikova@gmail.com>
2020-10-12 10:28:00 +0300

    Initial commit

---
+++ src/main/java/com/renart/renart/RenArtApplication.java
@@ -0,0 +1,13 @@
+package com.renart.renart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RenArtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RenArtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/renart/renart/RenArtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.renart.renart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RenArtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
